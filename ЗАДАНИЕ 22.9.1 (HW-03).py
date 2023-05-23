029cfcddacc7cbeaaaf99d327c79176240f56fba
RUSK84 <127926984+RUSK84@users.noreply.github.com>
2023-05-23 20:08:55 +0300

    Create ЗАДАНИЕ 22.9.1 (HW-03).py

---
+++ ЗАДАНИЕ 22.9.1 (HW-03).py
@@ -0,0 +1,58 @@
+import random
+ #Получаем список чисел
+while True: #Цыкл на проверку что числа целие в списке
+  try:
+     array = list(map(int, input('Введите целые числа (через пробел): ').split())) #Вводим последовательность чисел
+
+  except ValueError: #Делаем проверку на правильность ввода данных, если данные не соответствуют условиям повторяем ввод
+     print("!!!Ошибка ввода!!!")
+     array = list(map(int, input('Введите целые числа (через пробел): ').split()))
+  break
+
+#Цыкл на проверку что вводимое число целие
+while True:
+  try:
+     element = int(input('Введите целое числа : '))
+
+  except ValueError:
+     print("!!!Ошибка ввода!!!")
+     element = int(input('Введите целое числа : '))
+  break
+
+#Добавляем введенное число в список и сортируем по возрастанию
+array.append(element)
+
+def Sort(array):
+    if len(array) <= 1:
+        return array
+    else:
+        q = random.choice(array) #элемент q выбирается случайным образом из списка при помощи функции choice из модуля random
+        L = [] #собираются элементы, меньшие q
+        M = [] #равные q
+        R = [] #большие q
+        for elem in array:
+            if elem < q:
+                L.append(elem)
+            elif elem > q:
+                R.append(elem)
+            else:            
+                M.append(elem)
+        return Sort(L) + M + Sort(R)
+
+# def binary_search(array_mod, element, left, right):
+#     if left > right:  # если левая граница превысила правую,
+#         return False  # значит элемент отсутствует
+#
+#     middle = (right + left) // 2  # находимо середину
+#     if array_mod[middle] == element:  # если элемент в середине,
+#         return middle  # возвращаем этот индекс
+#     elif element < array_mod[middle]:  # если элемент меньше элемента в середине
+#             #  рекурсивно ищем в левой половине
+#         return binary_search(array_mod, element, left, middle - 1)
+#     else:  # иначе в правой
+#         return binary_search(array_mod, element, middle + 1, right)
+#
+
+# # запускаем алгоритм на левой и правой границе
+# print(binary_search(array_mod, element, 0, 99))
+# print (array_mod)
