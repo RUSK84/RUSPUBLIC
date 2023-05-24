f9de3851d38f54f9c239a7836c971ad001c90ae2
RUSK84 <127926984+RUSK84@users.noreply.github.com>
2023-05-24 14:04:41 +0300

    Update ЗАДАНИЕ 22.9.1 (HW-03).py

--- ЗАДАНИЕ 22.9.1 (HW-03).py
+++ ЗАДАНИЕ 22.9.1 (HW-03).py
@@ -21,8 +21,8 @@ while True:
 
 #Добавляем введенное число в список и сортируем по возрастанию
 array.append(element)
-
-def Sort(array):
+#Делаем быструю сортировку
+def QuickSort(array):
     if len(array) <= 1:
         return array
     else:
@@ -37,22 +37,31 @@ def Sort(array):
                 R.append(elem)
             else:            
                 M.append(elem)
-        return Sort(L) + M + Sort(R)
+        return QuickSort(L) + M + QuickSort(R)
 
-# def binary_search(array_mod, element, left, right):
-#     if left > right:  # если левая граница превысила правую,
-#         return False  # значит элемент отсутствует
-#
-#     middle = (right + left) // 2  # находимо середину
-#     if array_mod[middle] == element:  # если элемент в середине,
-#         return middle  # возвращаем этот индекс
-#     elif element < array_mod[middle]:  # если элемент меньше элемента в середине
-#             #  рекурсивно ищем в левой половине
-#         return binary_search(array_mod, element, left, middle - 1)
-#     else:  # иначе в правой
-#         return binary_search(array_mod, element, middle + 1, right)
-#
 
-# # запускаем алгоритм на левой и правой границе
-# print(binary_search(array_mod, element, 0, 99))
-# print (array_mod)
+array=QuickSort(array)
+print (array)
+def binary_search(array, element, left, right):
+    if left > right:  # если левая граница превысила правую,
+        return False  # значит элемент отсутствует
+
+    middle = (right + left) // 2  # находимо середину
+    if array[middle] == element:  # если элемент в середине,
+        return middle  # возвращаем этот индекс
+    elif element < array[middle]:  # если элемент меньше элемента в середине
+            #  рекурсивно ищем в левой половине
+        return binary_search(array, element, left, middle - 1)
+    else:  # иначе в правой
+        return binary_search(array, element, middle + 1, right)
+# запускаем алгоритм на левой и правой границе
+print(binary_search(array, element, 0, len(array)-1)) #Выводим номер введенного элемента
+
+position=binary_search(array, element, 0, len(array)-1) #Присваиваем полученное значение позиции новому аргументу
+#Выводим номера позиций элементов перед и после введенного значения
+if position == len(array)-1:
+    print(binary_search(array, element, 0, len(array)-1)-1, binary_search(array, element, 0, len(array)-1))
+elif position ==0:
+    print(binary_search(array, element, 0, len(array)-1), binary_search(array, element, 0, len(array)-1)+1)
+else:
+    print (binary_search(array, element, 0, len(array)-1)-1, binary_search(array, element, 0, len(array)-1)+1)
